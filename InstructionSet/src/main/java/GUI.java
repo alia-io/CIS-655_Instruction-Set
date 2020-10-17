a3c7ce38c7671b1cd2d929682b7c241fe77701b5
Alifa Stith <alifastith@gmail.com>
2020-10-16 20:10:35 -0400

    more gui stuff

--- InstructionSet/src/main/java/GUI.java
+++ InstructionSet/src/main/java/GUI.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GUI {
 
@@ -7,6 +11,10 @@ public class GUI {
     static JPanel registers = new JPanel();
     static JPanel instructions = new JPanel();
     static JPanel memory = new JPanel();
+    static JTextField instructionTextField = new JTextField(20);
+    static JButton runInstructionButton = new JButton("Run Instruction");
+    static JTextArea registerTextArea = new JTextArea(10, 30);
+    static JTextArea memoryTextArea = new JTextArea(10, 30);
 
     public static void runGUI() {
         setFrame();
@@ -21,12 +29,75 @@ public class GUI {
     }
 
     private static void setPanels() {
+        setRegisterPanel();
+        setInstructionPanel();
+        setMemoryPanel();
+    }
+
+    private static void setRegisterPanel() {
         registers.add(new JLabel("Register Panel"));
-        instructions.add(new JLabel("Instruction Panel"));
-        memory.add(new JLabel("Memory Panel"));
+        setRegisterTextArea();
         frame.getContentPane().add(registers, BorderLayout.WEST);
+    }
+
+    private static void setInstructionPanel() {
+        instructions.add(new JLabel("Instruction Panel"));
+        setInstructionTextField();
+        setRunInstructionButton();
         frame.getContentPane().add(instructions, BorderLayout.CENTER);
+    }
+
+    private static void setMemoryPanel() {
+        memory.add(new JLabel("Memory Panel"));
+        setMemoryTextArea();
         frame.getContentPane().add(memory, BorderLayout.EAST);
+    }
+
+    private static void setRegisterTextArea() {
+        registerTextArea.setText("text\ntext\ntext");
+        registerTextArea.setEditable(false);
+        registers.add(registerTextArea);
+    }
+
+    private static void setMemoryTextArea() {
+        memoryTextArea.setText("text\ntext\ntext");
+        memoryTextArea.setEditable(false);
+        memory.add(memoryTextArea);
+    }
+
+    private static void setInstructionTextField() {
+        instructions.add(instructionTextField);
+        instructionTextField.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) { }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    runInstruction();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) { }
+        });
+    }
+
+    private static void setRunInstructionButton() {
+        instructions.add(runInstructionButton);
+        runInstructionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                runInstruction();
+            }
+        });
+    }
+
+    private static void runInstruction() {
+        String instruction = instructionTextField.getText();
+        // TODO: run instruction
+        instructionTextField.setText("");
     }
 
 }
